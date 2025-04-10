383e2f6439376817e0552eeeabb8c4403e298fba
Christian Wirsching <christianwirsching@gmail.com>
2025-04-10 10:55:28 +0200

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { Route, Routes } from "react-router-dom"
+import { SignUpPage } from './pages/SignUpPage'
+import { LoginPage } from'./pages/LoginPage'
 
 function App() {
 
   return (
     <>
     <h1 className='text-7xl text-center'>Hello World</h1>
+    <Routes>
+    <Route path ="/Sign-up" element={<SignUpPage />} />
+    <Route path ="/Login" element={<LoginPage />} />
+    </Routes>
     </>
   )
 }
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import { LoginForm } from '@/components/login-form'
+import React from 'react'
+
+export const LoginPage = () => {
+  return (
+    <div>
+        <LoginForm />
+    </div>
+  )
+}
