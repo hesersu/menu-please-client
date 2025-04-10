d90810bfffcf6078e717a21787fdb2c14b229444
Krists Krievins <krists.krievins@gmail.com>
2025-04-10 11:54:37 +0200

    added AuthContext and Incomplete Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import './App.css'
-import { Route, Routes } from "react-router-dom"
-import { SignUpPage } from './pages/SignUpPage'
-import { LoginPage } from'./pages/LoginPage'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { SignUpPage } from "./pages/SignUpPage";
+import { LoginPage } from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
-
   return (
     <>
-    <h1 className='text-7xl text-center'>Hello World</h1>
-    <Routes>
-    <Route path ="/Sign-up" element={<SignUpPage />} />
-    <Route path ="/Login" element={<LoginPage />} />
-    </Routes>
+      <h1 className="text-7xl text-center">Hello World</h1>
+      <Routes>
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App  
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter as Browser } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Browser } from "react-router-dom";
+import { AuthContextWrapper } from "./contexts/authContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Browser>
-      <App />
+      <AuthContextWrapper>
+        <App />
+      </AuthContextWrapper>
     </Browser>
-  </StrictMode>,
-)
+  </StrictMode>
+);
