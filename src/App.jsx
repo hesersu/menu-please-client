c7dbbd2e1702d1c47ac43318090438d77425ace2
Christian Wirsching <christianwirsching@gmail.com>
2025-04-10 13:24:29 +0200

    navbar implemented

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import { SignUpPage } from "./pages/SignUpPage";
 import { LoginPage } from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
+import { Navbar1 } from "./components/navbar";
 
 function App() {
   return (
     <>
+      <Navbar1 />
       <Routes>
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
