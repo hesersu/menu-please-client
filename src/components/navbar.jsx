eccce3053b30cdda094200871428f9cdc568b507
Krists Krievins <krists.krievins@gmail.com>
2025-04-10 15:02:52 +0200

    Added paddings to menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,34 +1,34 @@
-import { Book, Menu, Sunset, Trees, Zap } from "lucide-react"
+import { Book, Menu, Sunset, Trees, Zap } from "lucide-react";
 
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
-  AccordionTrigger
-} from "@/components/ui/accordion"
-import { Button } from "@/components/ui/button"
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
 import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger
-} from "@/components/ui/navigation-menu"
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 import {
   Sheet,
   SheetContent,
   SheetHeader,
   SheetTitle,
-  SheetTrigger
-} from "@/components/ui/sheet"
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export const Navbar1 = ({
   logo = {
-    url: "https://www.shadcnblocks.com",
+    url: "/",
     src: "https://shadcnblocks.com/images/block/logos/shadcnblockscom-icon.svg",
     alt: "logo",
-    title: "Shadcnblocks.com"
+    title: "Shadcnblocks.com",
   },
 
   menu = [
@@ -41,28 +41,28 @@ export const Navbar1 = ({
           title: "Blog",
           description: "The latest industry news, updates, and info",
           icon: <Book className="size-5 shrink-0" />,
-          url: "#"
+          url: "#",
         },
         {
           title: "Company",
           description: "Our mission is to innovate and empower the world",
           icon: <Trees className="size-5 shrink-0" />,
-          url: "#"
+          url: "#",
         },
         {
           title: "Careers",
           description: "Browse job listing and discover our workspace",
           icon: <Sunset className="size-5 shrink-0" />,
-          url: "#"
+          url: "#",
         },
         {
           title: "Support",
           description:
             "Get in touch with our support team or visit our community forums",
           icon: <Zap className="size-5 shrink-0" />,
-          url: "#"
-        }
-      ]
+          url: "#",
+        },
+      ],
     },
     {
       title: "Resources",
@@ -72,46 +72,46 @@ export const Navbar1 = ({
           title: "Help Center",
           description: "Get all the answers you need right here",
           icon: <Zap className="size-5 shrink-0" />,
-          url: "#"
+          url: "#",
         },
         {
           title: "Contact Us",
           description: "We are here to help you with any questions you have",
           icon: <Sunset className="size-5 shrink-0" />,
-          url: "#"
+          url: "#",
         },
         {
           title: "Status",
           description: "Check the current status of our services and APIs",
           icon: <Trees className="size-5 shrink-0" />,
-          url: "#"
+          url: "#",
         },
         {
           title: "Terms of Service",
           description: "Our terms and conditions for using our services",
           icon: <Book className="size-5 shrink-0" />,
-          url: "#"
-        }
-      ]
+          url: "#",
+        },
+      ],
     },
     {
       title: "Pricing",
-      url: "#"
+      url: "#",
     },
     {
       title: "Blog",
-      url: "#"
-    }
+      url: "#",
+    },
   ],
 
   auth = {
     login: { title: "Login", url: "#" },
-    signup: { title: "Sign up", url: "#" }
-  }
+    signup: { title: "Sign up", url: "#" },
+  },
 }) => {
   return (
     <section className="py-4">
-      <div className="container">
+      <div>
         {/* Desktop Menu */}
         <nav className="hidden justify-between lg:flex">
           <div className="flex items-center gap-6">
@@ -125,7 +125,7 @@ export const Navbar1 = ({
             <div className="flex items-center">
               <NavigationMenu>
                 <NavigationMenuList>
-                  {menu.map(item => renderMenuItem(item))}
+                  {menu.map((item) => renderMenuItem(item))}
                 </NavigationMenuList>
               </NavigationMenu>
             </div>
@@ -134,7 +134,7 @@ export const Navbar1 = ({
             <Button asChild variant="outline" size="sm">
               <a href={auth.login.url}>{auth.login.title}</a>
             </Button>
-            <Button asChild size="sm">
+            <Button asChild size="sm" className="mr-8">
               <a href={auth.signup.url}>{auth.signup.title}</a>
             </Button>
           </div>
@@ -148,7 +148,7 @@ export const Navbar1 = ({
               <img src={logo.src} className="max-h-8" alt={logo.alt} />
             </a>
             <Sheet>
-              <SheetTrigger asChild>
+              <SheetTrigger asChild className="mr-4">
                 <Button variant="outline" size="icon">
                   <Menu className="size-4" />
                 </Button>
@@ -167,7 +167,7 @@ export const Navbar1 = ({
                     collapsible
                     className="flex w-full flex-col gap-4"
                   >
-                    {menu.map(item => renderMobileMenuItem(item))}
+                    {menu.map((item) => renderMobileMenuItem(item))}
                   </Accordion>
 
                   <div className="flex flex-col gap-3">
@@ -185,23 +185,23 @@ export const Navbar1 = ({
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-const renderMenuItem = item => {
+const renderMenuItem = (item) => {
   if (item.items) {
     return (
       <NavigationMenuItem key={item.title}>
         <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
         <NavigationMenuContent className="bg-popover text-popover-foreground">
-          {item.items.map(subItem => (
+          {item.items.map((subItem) => (
             <NavigationMenuLink asChild key={subItem.title} className="w-80">
               <SubMenuLink item={subItem} />
             </NavigationMenuLink>
           ))}
         </NavigationMenuContent>
       </NavigationMenuItem>
-    )
+    );
   }
 
   return (
@@ -213,10 +213,10 @@ const renderMenuItem = item => {
         {item.title}
       </NavigationMenuLink>
     </NavigationMenuItem>
-  )
-}
+  );
+};
 
-const renderMobileMenuItem = item => {
+const renderMobileMenuItem = (item) => {
   if (item.items) {
     return (
       <AccordionItem key={item.title} value={item.title} className="border-b-0">
@@ -224,20 +224,20 @@ const renderMobileMenuItem = item => {
           {item.title}
         </AccordionTrigger>
         <AccordionContent className="mt-2">
-          {item.items.map(subItem => (
+          {item.items.map((subItem) => (
             <SubMenuLink key={subItem.title} item={subItem} />
           ))}
         </AccordionContent>
       </AccordionItem>
-    )
+    );
   }
 
   return (
     <a key={item.title} href={item.url} className="text-md font-semibold">
       {item.title}
     </a>
-  )
-}
+  );
+};
 
 const SubMenuLink = ({ item }) => {
   return (
@@ -255,5 +255,5 @@ const SubMenuLink = ({ item }) => {
         )}
       </div>
     </a>
-  )
-}
+  );
+};
