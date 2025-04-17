a5929a698e5cedb99ca4717926122a74fb1d9d4d
Krists Krievins <krists.krievins@gmail.com>
2025-04-17 10:24:56 +0200

    Added BuyMeCoffee & Fixed Navbar spacing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ import { DarkModeToggle } from "@/components/darkmode-toggle";
 import LogoImage from "../assets/menu-please-logo.png";
 
 // Shadcn Components
-import { BookOpenText, CameraIcon, Menu } from "lucide-react";
+import { BookOpenText, CameraIcon, Heart, Menu } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -26,7 +26,7 @@ export const Navbar = ({}) => {
 
   // Page Content
   return (
-    <section className="py-6 c">
+    <section className="py-2">
       <div className="block mx-4">
         <div className="flex items-center justify-between">
           <a href="/translate-menu" className="flex items-center gap-2">
@@ -68,26 +68,21 @@ export const Navbar = ({}) => {
                 </div>
                 <Separator />
               </SheetHeader>
-              <div className="flex flex-col gap-6 px-4">
+              <div className="flex flex-col flex-grow gap-6 px-4">
                 <Link to="/translate-menu">
                   <h3 className="text-md py-0 font-semibold hover:no-underline flex items-center gap-3">
                     <CameraIcon /> Translate Menus
                   </h3>
                 </Link>
                 <Link to="/menu-history">
-                  <h3 className="text-md py-0 font-semibold hover:no-underline flex items-center gap-3">
+                  <h3 className="text-md py-0 font-semibold hover:no-underline flex gap-3">
                     <BookOpenText /> Menu history
                   </h3>
                 </Link>
                 <Separator className="my-3" />
-                <div className="flex flex-col gap-3 pt-3">
+                <div className="flex flex-col gap-3 pt-3 align-middle justify-center">
                   {isLoggedIn ? (
-                    <Button
-                      onClick={handleLogout}
-                      variant="outline"
-                      size="sm"
-                      className="mr-8"
-                    >
+                    <Button onClick={handleLogout} variant="outline" size="sm">
                       Logout
                     </Button>
                   ) : (
@@ -97,6 +92,21 @@ export const Navbar = ({}) => {
                       </Button>
                     </Link>
                   )}
+                </div>
+                <div className="px-4 mt-auto mb-12">
+                  <Separator className="my-5" />
+                  <h3 className="flex text-sm font-light justify-center text-center">
+                    Love "Menu, Please"? <br />
+                    Become a supporter!
+                  </h3>
+                  <a
+                    href="https://www.buymeacoffee.com/menuplease"
+                    className="flex justify-center w-full mt-3 scale-90"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src="https://img.buymeacoffee.com/button-api/?text=Buy us a coffee&emoji=&slug=menuplease&button_colour=eb52a9&font_colour=FFFFFF&font_family=&outline_colour=000000&coffee_colour=FFDD00" />
+                  </a>
                 </div>
               </div>
             </SheetContent>
