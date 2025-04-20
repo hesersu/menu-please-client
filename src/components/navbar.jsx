15e77c13c32a47ead13d8c501fd94a638ff5dcfb
Krists Krievins <krists.krievins@gmail.com>
2025-04-20 19:07:14 +0200

    Fixed Menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -76,85 +76,90 @@ export const Navbar = ({}) => {
               {currentPhrase.phrase}
             </h1>
           </div>
-          <Sheet>
-            <SheetTrigger asChild className="">
-              <Button variant="outline" size="icon">
-                <Menu className="size-4" />
-              </Button>
-            </SheetTrigger>
-            <SheetContent className="overflow-y-auto">
-              <SheetHeader>
-                <SheetTitle className="font-light text-sm"></SheetTitle>
-                {/* User Image */}
-                <div className="flex flex-row align-center mb-8">
-                  <Avatar className="h-15 w-15 mr-5 mt-10">
-                    <AvatarImage
-                      src={currentUser && currentUser.profileImage}
-                      alt={currentUser && currentUser.username}
-                    />
-                    <AvatarFallback>
-                      {currentUser && currentUser.username.charAt(0)}
-                    </AvatarFallback>
-                  </Avatar>
-                  <SheetDescription className="h-15 mr-5 mt-10 font-bold text-1xl content-center">
-                    Welcome, {currentUser && currentUser.username}!
-                  </SheetDescription>
-                </div>
-                <div className="flex flex-row justify-between align-center mb-5">
-                  <h3 className="text-md py-0 font-semibold hover:no-underline">
-                    Restaurant Mode
-                  </h3>
-                  <DarkModeToggle className="mr-5" />
-                </div>
-                <Separator />
-              </SheetHeader>
-              <div className="flex flex-col flex-grow gap-6 px-4">
-                <SheetClose asChild>
-                  <Link to="/translate-menu">
-                    <h3 className="text-md py-0 font-semibold hover:no-underline flex items-center gap-3">
-                      <CameraIcon /> Translate Menu
+          {isLoggedIn ? (
+            <Sheet>
+              <SheetTrigger asChild className="">
+                <Button variant="outline" size="icon">
+                  <Menu className="size-4" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="overflow-y-auto">
+                <SheetHeader>
+                  <SheetTitle className="font-light text-sm"></SheetTitle>
+                  {/* User Image */}
+                  <div className="flex flex-row align-center mb-8">
+                    <Avatar className="h-15 w-15 mr-5 mt-10">
+                      <AvatarImage
+                        src={currentUser && currentUser.profileImage}
+                        alt={currentUser && currentUser.username}
+                      />
+                      <AvatarFallback>
+                        {currentUser && currentUser.username.charAt(0)}
+                      </AvatarFallback>
+                    </Avatar>
+                    <SheetDescription className="h-15 mr-5 mt-10 font-bold text-1xl content-center">
+                      Welcome, {currentUser && currentUser.username}!
+                    </SheetDescription>
+                  </div>
+                  <div className="flex flex-row justify-between align-center mb-5">
+                    <h3 className="text-md py-0 font-semibold hover:no-underline">
+                      Restaurant Mode
                     </h3>
-                  </Link>
-                </SheetClose>
-                <SheetClose asChild>
-                  <Link to="/menu-history">
-                    <h3 className="text-md py-0 font-semibold hover:no-underline flex gap-3">
-                      <BookOpenText /> Menu history
-                    </h3>
-                  </Link>
-                </SheetClose>
-                <Separator className="my-3" />
-                <div className="flex flex-col gap-3 pt-3 align-middle justify-center">
-                  {isLoggedIn ? (
+                    <DarkModeToggle className="mr-5" />
+                  </div>
+                  <Separator />
+                </SheetHeader>
+                <div className="flex flex-col flex-grow gap-6 px-4">
+                  <SheetClose asChild>
+                    <Link to="/translate-menu">
+                      <h3 className="text-md py-0 font-semibold hover:no-underline flex items-center gap-3">
+                        <CameraIcon /> Translate Menu
+                      </h3>
+                    </Link>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Link to="/menu-history">
+                      <h3 className="text-md py-0 font-semibold hover:no-underline flex gap-3">
+                        <BookOpenText /> Menu history
+                      </h3>
+                    </Link>
+                  </SheetClose>
+                  <Separator className="my-3" />
+                  <div className="flex flex-col gap-3 pt-3 align-middle justify-center">
                     <Button onClick={handleLogout} variant="outline" size="sm">
                       Logout
                     </Button>
-                  ) : (
-                    <Link to="/login">
-                      <Button variant="outline" size="sm" className="mr-8">
-                        Login
-                      </Button>
-                    </Link>
-                  )}
+                  </div>
+                  <div className="px-4 mt-auto mb-12">
+                    <Separator className="my-5" />
+                    <h3 className="flex text-sm font-light justify-center text-center">
+                      Love "Menu, Please"? <br />
+                      Become a supporter!
+                    </h3>
+                    <a
+                      href="https://www.buymeacoffee.com/menuplease"
+                      className="flex justify-center w-full mt-3 scale-90"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src="https://img.buymeacoffee.com/button-api/?text=Buy us a coffee&emoji=&slug=menuplease&button_colour=eb52a9&font_colour=FFFFFF&font_family=&outline_colour=000000&coffee_colour=FFDD00" />
+                    </a>
+                  </div>
                 </div>
-                <div className="px-4 mt-auto mb-12">
-                  <Separator className="my-5" />
-                  <h3 className="flex text-sm font-light justify-center text-center">
-                    Love "Menu, Please"? <br />
-                    Become a supporter!
-                  </h3>
-                  <a
-                    href="https://www.buymeacoffee.com/menuplease"
-                    className="flex justify-center w-full mt-3 scale-90"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img src="https://img.buymeacoffee.com/button-api/?text=Buy us a coffee&emoji=&slug=menuplease&button_colour=eb52a9&font_colour=FFFFFF&font_family=&outline_colour=000000&coffee_colour=FFDD00" />
-                  </a>
-                </div>
+              </SheetContent>
+            </Sheet>
+          ) : (
+            <>
+              <div className="flex flex-row gap-3">
+                <DarkModeToggle className="mr-4" />
+                <Link to="/login">
+                  <Button variant="outline" size="sm">
+                    Login
+                  </Button>
+                </Link>
               </div>
-            </SheetContent>
-          </Sheet>
+            </>
+          )}
         </div>
       </div>
     </section>
